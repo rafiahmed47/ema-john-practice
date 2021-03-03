40e4655d2bc7569481e1831b905f7d3e2d3db1ba
rafiahmed47 <rafiahmed0471@gmail.com>
2021-03-04 04:24:28 +0600

    started with router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Shop from "./components/shop/Shop";
+import Review from "./components/review/Review";
+import Manage from "./components/manage/Manage";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <Review></Review>
+          </Route>
+          <Route path="/manage">
+            <Manage></Manage>
+          </Route>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route>
+
+          </Route>
+        </Switch>
+      </Router>
+
+
     </div>
   );
 }
---
+++ src/components/manage/Manage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Manage = () => {
+    return (
+        <div>
+            <h1>Developer is sleeping</h1>
+        </div>
+    );
+};
+
+export default Manage;
